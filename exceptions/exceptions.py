b7e86563a7f1d980fe7308e648319f527443888a
Youngseo Kim <60006301+youngseo0526@users.noreply.github.com>
2022-10-26 20:29:58 +0900

    20221026

---
+++ exceptions/exceptions.py
@@ -0,0 +1,11 @@
+class BaseSimCLRException(Exception):
+    """Base exception"""
+
+
+class InvalidBackboneError(BaseSimCLRException):
+    """Raised when the choice of backbone Convnet is invalid."""
+
+
+
+class InvalidDatasetSelection(BaseSimCLRException):
+    """Raised when the choice of dataset is invalid."""
